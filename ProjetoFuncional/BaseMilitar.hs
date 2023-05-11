ac00998fdd26252285e4a2349d6f12f794b3e819
Luiz Sergio Pompeu <95686568+LS-Flerus@users.noreply.github.com>
2023-05-11 16:06:33 -0300

    Update BaseMilitar.hs

--- ProjetoFuncional/BaseMilitar.hs
+++ ProjetoFuncional/BaseMilitar.hs
@@ -5,8 +5,26 @@ import Itens
 import Inventario
 
 baseMilitar = Local{ 
-        nomeLocal = "Base Militar"
-        , inventarioLocal = []
-        , eventosInvestigacao = []
-        , eventosColeta = []
-                    }
+          nomeLocal = "BaseMilitar",
+          inventarioLocal = [(linha, 200), (bateriaCarro,1), (lanterna, 10)],
+          eventosInvestigacao = [EventoInvestigacao {
+                                      -- Evento de sucesso
+                                requisitosInvestigacao = [lanterna],
+                                alvoInvestigacao = [bateriaCarro],
+                                mensagemFalha = "Voce toma coragem para entrar na base militar, mas estah escuro dentro e voce tem medo do escuro. Uma pena mesmo",
+                                mensagemSucesso = "Voce entra na base militar. Tem um robo lah, dancando a macarena. Ele diz que lhe da uma bateria de carro se você for coletar depois."
+                                      
+                }],
+        eventosColeta = 
+            [EventoColeta {
+                    requisitosColeta = [],
+                    coletado = lanterna 
+                },
+                EventoColeta {
+                    requisitosColeta = [],
+                    coletado = linha 
+                },
+                EventoColeta {
+                    requisitosColeta = [lanterna],
+                    coletado = bateriaCarro 
+               }]}
