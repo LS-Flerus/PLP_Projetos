ca14349e420636dc0af7251ab97a967da6dac0bc
Gabriel Oliveira Rodrigues <111239856+GabrielRodrigues-42@users.noreply.github.com>
2023-05-11 16:07:17 -0300

    Add files via upload

--- ProjetoFuncional/BaseMilitar.hs
+++ ProjetoFuncional/BaseMilitar.hs
@@ -1,12 +1,20 @@
-module BaseMilitar where
-
-import Locais
-import Itens
-import Inventario
-
-baseMilitar = Local{ 
-        nomeLocal = "Base Militar"
-        , inventarioLocal = []
-        , eventosInvestigacao = []
-        , eventosColeta = []
-                    }
+BaseMilitar = Local{ 
+          nomeLocal = "BaseMilitar",
+          inventarioLocal = [(linha, 200), (bateriaCarro,1)],
+          eventosInvestigacao = [EventoInvestigacao {
+                                      -- Evento de sucesso
+                                requisitosInvestigacao = [lanterna],
+                                alvoInvestigacao = [bateriaCarro],
+                                mensagemFalha = "Voce toma coragem para entrar na base militar, mas estah escuro dentro e voce tem medo do escuro. Uma pena mesmo",
+                                mensagemSucesso = "Voce entra na base militar. Tem um robo lah, dancando a macarena. Ele diz que lhe da uma bateria de carro se você for coletar depois."
+                                      
+                }],
+        eventosColeta = 
+            [EventoColeta {
+                    requisitosColeta = [],
+                    coletado = lanterna 
+                },
+                EventoColeta {
+                    requisitosColeta = [lanterna],
+                    coletado = bateriaCarro 
+               }]}
--- ProjetoFuncional/Lago.hs
+++ ProjetoFuncional/Lago.hs
@@ -1,57 +1,24 @@
-module Lago where
-import Locais
-import Itens
-import Inventario
-
-lago = Local{ 
-          nomeLocal = "Lago"
-        , inventarioLocal = listaParaInventario [pedra, pedra, madeira, comida, agua, agua]
-        , eventosInvestigacao = 
-            [
-                EventoInvestigacao 
-                { -- a mensagem do deus das frutas tropicais
-                    requisitosInvestigacao = [lanterna]
-                    ,alvoInvestigacao = [isqueiro]
-                    ,mensagemFalha = "Ha um lugar mais escuro do que todos os outros nesse lago: uma arvore morta, de tronco oco. Ha um buraco na base, onde você poderia enfiar o braco para procurar itens. Dá para ouvir alguma coisa se mexendo lá dentro.\nVoce julga melhor voltar aqui com uma lanterna, ate para poder driblar a ira do que for que esta morando la."
-
-                    , mensagemSucesso = "De lanterna em maos, voce vai direto para o tronco oco do lago. -Que se exploda o que estiver la dentro,- voce pensa. Com um tiquinho assim de apreensao, voce se deita e aponta a lanterna para dentro do buraco...\n\nNada. Nada vivo, ao menos: da para ver uns restos gelatinosos que foram comida de algum bicho pequeno e nojento. La dentro, depois dos restos gosmentos, um papel:\n\n''''Nao temos coqueiro por aqui, mas a ajuda esta num coco.\n O coco eh seu amigo, mas ele eh dificil de achar.\n Ele nao tem >>>FACA<<< para se soltar das vinhas\n  Muito menos uma >>>LANTERNA<<< para ver entre as linhas''''\n\nVoce nao entende essa mensagem, mesmo com as letras maisculas garrafais e os sinais para duas palavras especificas. De qualquer forma, eh bom saber que alguem se importa com voce no meio desse nada: o deus das frutas tropicais esta do seu lado."
+Lago = Local{ 
+          nomeLocal = "Lago",
+          inventarioLocal = [(agua, 200), (estepe,1)],
+          eventosInvestigacao = [EventoInvestigacao {
+                                      -- Evento de sucesso
+                                requisitosInvestigacao = [varaPesca],
+                                alvoInvestigacao = [estepe],
+                                mensagemFalha = "O lago eh confortante, abundante em agua e brisa, um espaco de calmaria no mar de caos que se tornou sua vida. Voce passa um tempo apenas atirando pedrinhas na agua limpida, sentindo a pressao se perder em cada salto, contando as ondulaccoes e sentindo sua respiraccao estabilizar. Pensando claramente, pedacco a pedacco, sua esperancca volta. E, assim, voce nota algo no fundo, um circulo preto no fundo... serah? Estah muito profundo para nadar, mas se voce tivesse alguma coisa para alcanccar.... Na proxima vez, voce virah preparado.",
+                                mensagemSucesso = "Tem um pneu no fundo do lago, mas seus bracinhos de macarrao nao tem forcca para pescah-lo. Volte sem a mochila para super-poderes de carregamento"
                                       
+                }],
+        eventosColeta = 
+            [EventoColeta {
+                    requisitosColeta = [varaPesca],
+                    coletado = estepe 
                 },
-                EventoInvestigacao 
-                { -- a mensagem do deus das frutas tropicais
-                    requisitosInvestigacao = [faca]
-                    , alvoInvestigacao = [estepe]
-                    , mensagemFalha = "Num surto de sorte, voce acha uma canoa de pesca. Sonhos de fuga passam pela sua cabeca, ate voce lembrar que isso eh um lago: o maximo que voce pode fazer eh dar um passeio e voltar. Mas algo te diz que ha segredos dentro desse lago, entao por que nao? Voce ate tenta embarcar na canoa, mas um detalhe tinha passado despercebido ate entao:\n  A canoa esta presa ao pier com uma corda. Uma corda muito forte, do tipo que so uma Faca poderia cortar."
-
-                    , mensagemSucesso = "Voce, com sua faca em maos, anda ate o pier do lago, onde uma corda forte prende a canoa. Voce desce a faca uma, duas, tres vezes e a corda se parte sem fazer um som. Com a canoa solta e nada pra te impedir, voce embarca na sua aventura de exploracao do pequeno corpo de agua.\n Voce procura um pouco e nada. Voce procura com mais vigor e ainda nada. Em um momento que voce se permite olhar (derrotado) para baixo, uma surpresa:\n Algo circular, com um aro metalico, refletindo luz, bem no fundo do lago. Esta fundo, mas nao fundo o suficiente que uma Vara de Pesca nao seja capaz de alcancar"
-                                      
+                EventoColeta {
+                    requisitosColeta = [],
+                    coletado = agua 
                 },
-                EventoInvestigacao 
-                { -- um quartinho de ferramentas na beira do lago, com a porta emperrada
-                    requisitosInvestigacao = [] -- nao precisa de nenhum item
-                    , alvoInvestigacao = [chapeuOperario] 
-                    , mensagemFalha = ""
-
-                    , mensagemSucesso = "Ha um pequeno galpao de ferramentas meio distante. Com toda a coragem e disposicao de quem quer sobreviver, voce anda sobre pedras e uma garrafa de vidro quebrada para chegar a porta do puxadinho. Alguma coisa (o vento, talvez?) fez o teto desabar, destruindo tudo e bloqueando a porta, deixando-a com um rombo.\n Pelo buraco da porta, voce ve tudo destruido, exceto por um unico chapeu de operario, que pousou no topo das prateleiras caídas.\n Imagine o nivel de protecao cranial que esse capacete ira lhe prover, meu amigo. Uma pena que a porta nao mexe um centimetro gracas aos escombros.\n Talvez com um Machado voce conseguisse quebrar a porta e adquirir uma cobertura para o cranio."
-                                      
-                }
-            ]
-        , eventosColeta = 
-            [
-                EventoColeta 
-                {
-                      requisitosColeta = [lanterna, faca] -- o tal do coco da mensagem acima
-                    , coletado = isqueiro
-                },
-                EventoColeta 
-                {
-                      requisitosColeta = [varaPesca] -- tem um pneu velho jogado dentro do lago
-                    , coletado = estepe
-                },
-                EventoColeta 
-                {
-                      requisitosColeta = [machado] -- o chapeu de Operario dentro de um pequeno galpao de ferramentas
-                    , coletado = chapeuOperario 
-                }
-            ]
-                   }
+                EventoColeta {
+                    requisitosColeta = [],
+                    coletado = madeira 
+                } ] } 
